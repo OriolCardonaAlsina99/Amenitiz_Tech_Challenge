1a47e9097e3ed8bbbb1cfe573c862bbc20673265
WanderingPhantom <oriol.cardona.alsina@gmail.com>
2024-10-11 15:11:10 +0200

    created COO and VP rules

--- cash_register.py
+++ cash_register.py
@@ -15,12 +15,51 @@ def CEO_rule (products):
         if(p.getId() == 'GR1'):
             green_teas.append(p)
             price += p.getPrice()
+    if (len(green_teas) < 2):
+        return price
     if(len(green_teas)%2 == 0):
         return price/2
     else:
-        green_tea_price = products[0].getPrice()
+        green_tea_price = green_teas[0].getPrice()
         size_arr = len(green_teas) - 1
-        final_price = (green_tea_price*size_arr)/2 + green_tea_price
+        return ((green_tea_price*size_arr)/2 + green_tea_price)
+
+def COO_rule(products):
+    strawberries = []
+    price = 0
+    final_price = 0
+    for p in products:
+        if(p.getId() == 'SR1'):
+            strawberries.append(p)
+            price += p.getPrice()
+    size_arr = len(strawberries)
+    if (size_arr  < 3): 
+        return price
+    else: 
+        strawberries_price = strawberries[0].getPrice()
+        while(size_arr%3 != 0):
+            size_arr -= 1
+            final_price += strawberries_price
+        final_price += 4.5*3*(size_arr/3)
+        return final_price
+    
+def VP_rule(products):
+    coffies = []
+    price = 0
+    final_price = 0
+    for p in products:
+        if(p.getId() == 'CF1'):
+            coffies.append(p)
+            price += p.getPrice()
+    size_arr = len(coffies)
+    if (size_arr  < 3): 
+        return price
+    else: 
+        coffies_price = coffies[0].getPrice()
+        while(size_arr%3 != 0):
+            size_arr -= 1
+            final_price += coffies_price
+        final_price += (coffies_price*size_arr*2)/3
         return final_price
 
 print ('| Product Code | Name | Price |') 
@@ -39,4 +78,8 @@ while True:
 
 price_rule1 = CEO_rule(products)
 total_price += price_rule1
+price_rule2 = COO_rule(products)
+total_price += price_rule2
+price_rule3 = VP_rule(products)
+total_price += price_rule3
 print_total(products, total_price)
