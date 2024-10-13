e040039040783a806e2ca857909263e58d39c37f
OriolCardonaAlsina99 <57458467+OriolCardonaAlsina99@users.noreply.github.com>
2024-10-13 13:09:52 +0200

    Delete src/rules directory

--- src/rules/check_basket.py
+++
@@ -1,5 +0,0 @@
-def handle_basket (products, total_price):
-    for p in products:
-        if (p.getId() != 'GR1' and p.getId() != 'SR1' and p.getId() != 'CF1'):
-            total_price += p.getPrice()
-    return total_price
