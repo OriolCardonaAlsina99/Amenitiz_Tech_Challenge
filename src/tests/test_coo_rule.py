7122b1d8ff9e3486635d957ffb0b199c71de2df4
OriolCardonaAlsina99 <57458467+OriolCardonaAlsina99@users.noreply.github.com>
2024-10-14 21:11:00 +0200

    Update test_coo_rule.py

--- src/tests/test_coo_rule.py
+++ src/tests/test_coo_rule.py
@@ -7,6 +7,7 @@ from modules.coo_rule import COO_rule
 
 class TestCOORule (unittest.TestCase):
 
+    # Test for the edge case of not enough products to test the discount
     def test_no_offer(self):
         prod1 = Product('SR1', 'Strawberries', 5)
         prod2 = Product('SR1', 'Strawberries', 5)
@@ -14,6 +15,7 @@ class TestCOORule (unittest.TestCase):
         result = COO_rule(arr_prods)
         self.assertEqual(result, 10)
 
+    # Test when we are have enough products for a discount
     def test_especial_offer(self):
         prod1 = Product('SR1', 'Strawberries', 5)
         prod2 = Product('SR1', 'Strawberries', 5)
@@ -22,11 +24,13 @@ class TestCOORule (unittest.TestCase):
         result = COO_rule(arr_prods)
         self.assertEqual(result, 13.5)
 
+    # Test when no products are introduced
     def test_no_products(self):
         arr_prods = []
         result = COO_rule(arr_prods)
         self.assertEqual(result, 0)
-    
+
+    # Test when there are enough products that some do not apply for discount
     def test_many_products(self):
         prod = Product('SR1', 'Strawberries', 5)
         arr_prods = [prod] * 8
