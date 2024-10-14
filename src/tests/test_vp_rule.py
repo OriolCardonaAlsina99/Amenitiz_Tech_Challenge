63f8295f4afdba6b0c06073221f7041f34b12887
OriolCardonaAlsina99 <57458467+OriolCardonaAlsina99@users.noreply.github.com>
2024-10-14 21:11:42 +0200

    Update test_vp_rule.py

--- src/tests/test_vp_rule.py
+++ src/tests/test_vp_rule.py
@@ -7,12 +7,14 @@ from modules.vp_rule import VP_rule
 
 class TestVPRule (unittest.TestCase):
 
+    # Test for the edge case of not enough products to test the discount
     def test_no_offer(self):
         prod = Product('CF1', 'Coffee', 11.23)
         arr_prods = [prod]
         result = VP_rule(arr_prods)
         self.assertEqual(result, 11.23)
 
+    # Test when we are have enough products for a discount
     def test_especial_offer(self):
         prod1 = Product('CF1', 'Coffee', 11.23)
         prod2 = Product('CF1', 'Coffee', 11.23)
@@ -21,11 +23,13 @@ class TestVPRule (unittest.TestCase):
         result = VP_rule(arr_prods)
         self.assertEqual(result, 22.46)
 
+    # Test when no products are introduced
     def test_no_products(self):
         arr_prods = []
         result = VP_rule(arr_prods)
         self.assertEqual(result, 0)
-    
+
+    # Test when there are enough products that some do not apply for discount
     def test_many_products(self):
         prod = Product('CF1', 'Coffee', 11.23)
         arr_prods = [prod] * 7
